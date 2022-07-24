25978c495c71e3137bd748c202878e85b1d2edb0
zaEp <istratkinss@mail.ru>
2022-07-25 02:07:00 +0300

    add new folder and all tasks of seminar_6

---
+++ Seminars/HomeWork/CSharp/HWTask41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+namespace Sem6Task41
+{
+    class Program
+    {
+        static void Main(string[] asgs)
+        {
+            Console.Write("Введите положительные или отрицательные числа, через пробел: ");
+            int[] numberEnd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < numberEnd.Length; i++)
+            {
+                Console.Write(numberEnd[i] + " | ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Вы ввели {GetPlus(numberEnd)} положительных чисел");
+        }
+        public static int[] GetIntArray(string[] array)
+        {
+            int[] arrayEnd = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayEnd[i] = int.Parse((array[i]).ToString());
+            }
+            return arrayEnd;
+        }
+        public static int GetPlus(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    count = count + 1;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Seminars/HomeWork/CSharp/HWTask43/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, 
+// y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+namespace Sem6Task43
+{
+    class Program
+    {
+        static void Main(string[] asgs)
+        {
+            Console.WriteLine("Поиск точки пересечения двух прямых");
+            Console.Write("Введите точку k1: ");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите точку b1: ");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите точку k2: ");
+            double k2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите точку b2: ");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double[] arrayXY = GetPoint(k1, b1, k2, b2);
+            Console.WriteLine($"Координаты точки пересечения x = {arrayXY[0]}, y = {arrayXY[1]}");
+        }
+
+        public static double[] GetPoint(double k1, double b1, double k2, double b2)
+        {
+            double[] arrayPoint = new double[2];
+            double x;
+            double y;
+            x = (b2 - b1) / (k1 - k2);
+            y = k1 * x + b1;
+            arrayPoint[0] = x;
+            arrayPoint[1] = y;
+            return arrayPoint;
+        }
+    }
+}
