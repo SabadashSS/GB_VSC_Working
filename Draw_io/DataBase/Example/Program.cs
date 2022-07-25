6db33d316c5c61c4958ebc6d269e9e845bae49cf
zaEp <istratkinss@mail.ru>
2022-07-25 23:40:24 +0300

    add new foder draw.io

---
+++ Draw_io/DataBase/Example/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "id;fName;lName;mark;status;description;\n";
+
+int count = 10;
+string[] status = { "Трус", "Балбес", "Бывалый" };
+string[] descriptions = { "Что-то написано", "Такие дела", "Уруру" };
+
+Random random = new Random();
+for (int id = 1; id <= count; id++)
+{
+    string fName = $"Иия_{id}";
+    string lName = $"Фамилия_{id}";
+    int mark = random.Next(2, 5) + 1;
+    string cStatus = status[random.Next(status.Length)];
+    string description = descriptions[random.Next(descriptions.Length)];
+    text += $"{id};{fName};{lName};{mark};{cStatus};{description};\n";
+}
+
+System.Console.WriteLine(text);
+File.WriteAllText("output.csv", text);
