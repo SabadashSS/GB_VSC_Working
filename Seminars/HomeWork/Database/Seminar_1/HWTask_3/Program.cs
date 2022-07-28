a9583f7ae747025be298de69477c2e47ee5172b8
zaEp <istratkinss@mail.ru>
2022-07-28 03:13:38 +0300

    added changes database HW 3

--- Seminars/HomeWork/Database/Seminar_1/HWTask_3/Program.cs
+++ Seminars/HomeWork/Database/Seminar_1/HWTask_3/Program.cs
@@ -7,23 +7,25 @@
 // Более сложные/глубокие структуры пока лучше не брать.
 
 
-string text = "id;fName;lName;mark;status;description;\n";
+string text = "id;route;mark;transP;driverR;\n";
 
 int count = 5;
 string[] status = { "На маршруте", "В Парке", "В ремонте" };
-string[] driver = { "Иванов И И", "Петров П П", "Сидоров С С"};
-string[] transport = { "Автобус", "Тролейбус", "Маршрутка"};
+string[] driver = { "Иванов И И", "Петров П П", "Сидоров С С" };
+string[] transport = { "Автобус", "Тролейбус", "Маршрутка" };
 
 Random random = new Random();
 for (int id = 1; id <= count; id++)
 {
-    string route = $"Маршрут: {id}";
+    string route = $"Маршрут:";
     string transP = $"Вид транспорта: {transport[random.Next(transport.Length)]}";
-    string condId = $"Кондуктор {condId[random.Next(condId.Length)]}";
+    string condId = $"Кондуктор: {id}";
     int mark = random.Next(100, 1000);
-    string cStatus = status[random.Next(status.Length)];
-    string description = driver[random.Next(driver.Length)];
-    text += $"{id}) {route} {transP} {mark} {cStatus} {description} {condId}\n";
+    string statusR = status[random.Next(status.Length)];
+    string driverR = driver[random.Next(driver.Length)];
+    string transportR = transport[random.Next(transport.Length)];
+    text += $"{id} {route} {mark} {transP} {driverR} {condId}\n";
+    // {description} {condId}\n";
 }
 
 System.Console.WriteLine(text);
